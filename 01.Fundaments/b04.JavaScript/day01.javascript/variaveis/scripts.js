9fca94b5d4466069678a632a5778d64a33277db3
Rafael Godoy <rafa.qfg@gmail.com>
2021-11-26 15:44:01 -0300

    day01.java

---
+++ 01.Fundaments/b04.JavaScript/day01.javascript/variaveis/scripts.js
@@ -0,0 +1,8 @@
+const myName = "Rafael";
+const birthCity = "Campinas";
+let birthYear = 1988;
+birthYear = 2030;
+birthCity = Araras;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);

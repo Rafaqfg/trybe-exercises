8a34ff31c7913616dff0a43bcaab617435c5974b
Rafael Godoy <rafa.qfg@gmail.com>
2021-11-28 12:21:59 -0300

    exercises 1 to 5

---
+++ 01.Fundaments/b04.JavaScript/day01.javascript/exercises/script.js
@@ -0,0 +1,57 @@
+// exercise 1.
+// let a = 3;
+// let b = 6;
+// console.log("soma " + (a + b));
+// console.log('subtração ' + (a - b));
+// console.log('multiplicação ' + (a * b));
+// console.log('divisão ' + (a / b));
+// console.log('módulo ' + (a % b));
+
+// exercise 2.
+// const a = 10;
+// const b = 20;
+// if (a > b) {
+// 	console.log("'a' é maior que 'b'")
+// } else {
+// 	console.log("'b' é maior que 'a'")
+// }
+
+// exercise 3.
+// let a = 50;
+// let b = 40;
+// let c = 30;
+// if (a > b && a > c) {
+// 	console.log(a);
+// } else if (b > a && b > c) {
+// 	console.log(b);
+// }else {
+// 	console.log(c);
+// }
+
+// exercise 4.
+// const a = -1;
+// if (a > 0) {
+// 	console.log('positivo')
+// } else if (a < 0) {
+// 	console.log('negativo')
+// } else {
+// 	console.log('0')
+// }
+
+// exercise 5.
+// const angOne = 60;
+// const angTwo = 60;
+// const angThree = 60;
+// let somaAngs = angOne + angTwo + angThree;
+// let todosPositivos = angOne > 0 && angTwo > 0 && angThree > 0;
+// if (todosPositivos){
+// 	if(somaAngs === 180) {
+// 		console.log(true);
+// 	} else {
+// 		console.log(false);
+// 	};
+// } else {
+// 	console.log('ângulo inválido')
+// }
+
+// exercise 6.

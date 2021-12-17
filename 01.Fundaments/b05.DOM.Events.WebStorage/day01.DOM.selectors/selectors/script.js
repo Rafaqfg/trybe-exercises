c4d9cdda4a9542131fa2d7b028fff1bb36b199f5
Rafael Godoy <rafa.qfg@gmail.com>
2021-12-17 09:27:24 -0300

    selectors 1

---
+++ 01.Fundaments/b05.DOM.Events.WebStorage/day01.DOM.selectors/selectors/script.js
@@ -0,0 +1,6 @@
+document.getElementById("page-title").innerHTML = "vários";
+document.getElementById("paragraph").style.color = "red";
+document.getElementById("second-paragraph").style.backgroundColor = "blue";
+document.getElementsByTagName('h4')[0].innerHTML = "tagname!";
+document.getElementsByClassName("p")[0].style.fontSize = "50px";
+document.getElementsByTagName('h4')[0].style.color = "green";

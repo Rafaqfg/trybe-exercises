1d194ad6b9c8da1ff754134fb3af49e0ff5a54b0
Rafael Godoy <rafa.qfg@gmail.com>
2022-01-06 13:55:06 -0300

    finaliza criançao lista de estados

--- 01.Fundamentals/b06.Forms.Flexbox.Responsivo/day01.Forms/curriculumForm/script.js
+++ 01.Fundamentals/b06.Forms.Flexbox.Responsivo/day01.Forms/curriculumForm/script.js
@@ -1,2 +1,12 @@
-let stateselector = document.getElementById("estadouser");
-let states = [AC, AL, AP, AM, BA, CE, DF, ES, GO, MA, MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO];
+function createStatesOptions() {
+  let stateSelector = document.getElementById("estadouser");
+  let states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
+  for (i = 0; i < states.length; i += 1) {
+    let option = document.createElement('option');
+    option.innerText = states[i];
+    option.value = states[i];
+    stateSelector.appendChild(option);
+  }
+}
+createStatesOptions()

e935a653ce7124cdbb80e8261f8dd211923a04fc
Rafael Godoy <rafa.qfg@gmail.com>
2022-01-06 13:32:47 -0300

    criando estados

---
+++ 01.Fundamentals/b06.Forms.Flexbox.Responsivo/day01.Forms/curriculumForm/script.js
@@ -0,0 +1,2 @@
+let stateselector = document.getElementById("estadouser");
+let states = [AC, AL, AP, AM, BA, CE, DF, ES, GO, MA, MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO];
